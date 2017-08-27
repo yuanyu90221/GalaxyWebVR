9ac48f6e3b2396d85306d33b3310ac2190ffcae3
yuanyu90221 <yuanyu90221@gmail.com>
2017-08-27 13:47:24 +0800

    new update

--- public/js/animation.js
+++ public/js/animation.js
@@ -2,7 +2,15 @@ $(document).ready(function(){
     
 });
 var earthText = null;
+var ca = document.querySelector('#ca');
+var beginMessage = document.querySelector('#beginMessage');
+var launchMusic = document.querySelector('#launchMusic');
+var showMessage2 = document.querySelector('#showMessage2');
+var triggerTimer = null;
 var checkArray = {
+ "Lee":0,
+ "DoFu":0,
+ "Wang":0,
  "Earth":0,
  "Mercury":0,
  "Venus":0,
@@ -13,6 +21,7 @@ var checkArray = {
  "Neptune":0,
  "Sun":0
 };
+var prev = null;
 AFRAME.registerComponent('show-text',{
     schema: {
         text: {default:'no data'}
@@ -24,22 +33,62 @@ AFRAME.registerComponent('show-text',{
             console.log(data.text);
             earthText = document.querySelector('#showMessage1');
             earthText.setAttribute('value',data.text);
+            launchMusic = document.querySelector('#launchMusic');
+            ca = document.querySelector('#ca');
+            beginMessage = document.querySelector('#beginMessage');
             // earthText.setAttribute('opacity',1);
             setTimeout( function(){
                 // var earthText = document.querySelector('#showMessage1');
                 earthText.setAttribute('value', '');
             },1000);
             checkArray[`${data.text}`]++;
-            if(checkArray['Earth']>= 2 && data.text=="Earth"){
+            prev = data.text;
+            if(checkArray['Mars']>= 2 && data.text=="Mars"){
                 // document.querySelector('#galaxy-scene').setAttribute('visible',false);
                 // document.querySelector('#landing').setAttribute('visible',true);
                 location.href = './landing/landing.html';
+            }
+            else {
+                if((checkArray['Lee']>= 1 && data.text=="Lee") || (checkArray['DoFu']>= 1 && data.text=="DoFu") || (checkArray['Wang']>= 1 && data.text=="Wang")){
+                    // beginMessage.setAttribute('visible',false);
+                    // showMessage2 = document.querySelector('#showMessage2');
+                    // showMessage2.setAttribute('value',data.text+' said go to Mars');
+                    // earthText.setAttribute('value','');
+                    // setTimeout( function(){
+                    //     // var earthText = document.querySelector('#showMessage1');
+                    //     showMessage2.setAttribute('value', '');
+                    // },15000);
+                    // ca.emit('fade');
+                    
+                    triggerTimer = setTimeout( function(){
+                        if(prev == data.text){
+                          moveIn();
+                        }
+                        clearTimeout(triggerTimer);
+                    }, 2000);
+                    // moveIn();
+                    // launchMusic.setAttribute('autoplay',true);
+                    // setTimeout( function(){
+                        
+                    //     launchMusic.setAttribute('autoplay',false);
+                    // },25000);
+                }
             }
            
         });
     }
 });
-
+function moveIn(){
+    beginMessage.setAttribute('visible',false);
+    showMessage2 = document.querySelector('#showMessage2');
+    showMessage2.setAttribute('value',prev+' said go to Mars');
+    earthText.setAttribute('value','');
+    setTimeout( function(){
+        // var earthText = document.querySelector('#showMessage1');
+        showMessage2.setAttribute('value', '');
+    },15000);
+    ca.emit('fade');
+}
 // AFRAME.registerComponent('open-vr',{
 //     init: function(){
 //         console.log('openVR');
--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,43 @@
 var rockWrapTop = document.querySelector('#rockWrapTop');
 var rockWrapLeft = document.querySelector('#rockWrapLeft');
 var rockWrapRight = document.querySelector('#rockWrapRight');
+var rockWrapBottom = document.querySelector('#rockWrapBottom');
 var player = document.querySelector('#player');
 var maze = document.querySelector('#maze');
+var mazeWall1 = document.querySelector('#wall1');
+var mazeWall2 = document.querySelector('#wall2');
+var mazeWall3 = document.querySelector('#wall3');
+var mazeWall4 = document.querySelector('#wall4');
 var opening = document.querySelector('#opening');
 var rockCorner;
 var rockwall;
 var corner = 0;
 var hoverCheck = false;
+var treasureCheck = false;
 var MAP_SIZE = 10,
 PLATFORM_SIZE = 5,
-NUM_PLATFORMS = 100;
+NUM_PLATFORMS = 35;
 var v, rock;
 var currntposZ;
 var currntposX;
 var currntposY;
-var treasureXPos = 0;
-var treasureZPos = 35;
+var frontAndleft = Math.floor(Math.random() * 2)
+var treasureXPos;
+var treasureZPos;
+
+if(frontAndleft > 1){
+  treasureXPos = Math.floor(Math.random() * 10);
+  treasureZPos = Math.floor(Math.random() * 20);
+}else{
+  treasureXPos = Math.floor(Math.random() * 10)*-1;
+  treasureZPos = Math.floor(Math.random() * 20)*-1;
+}
 document.querySelector('a-scene').addEventListener('render-target-loaded',function(){
 
   loadrockWall(rockWrapTop);
   loadrockWall(rockWrapLeft);
   loadrockWall(rockWrapRight);
+  loadrockWall(rockWrapBottom);
   loadMaze();
 
 
@@ -31,7 +47,10 @@ function loadrockWall(side){
 
   for(var i=0;i<15;i++){
       rockwall = document.createElement('a-dodecahedron');
+      // rockwall =  document.createElement('a-entity');
       side.appendChild(rockwall);
+      // rockwall.setAttribute('obj-model','obj: #rockObj; mtl: #rockMtl');
+      // rockwall.setAttribute('scale','0.08 0.08 0.08');
       rockwall.setAttribute('src','#stone');
       rockwall.setAttribute('radius','5.5');
       rockwall.setAttribute('static-body','');
@@ -40,31 +59,63 @@ function loadrockWall(side){
 }
 
 function loadMaze(){
-  for (var i = 0;  i < NUM_PLATFORMS; i++) {
+  for (var i = 0;  i < 20; i++) {
       // y: 0 is ground
+      var wallnumber = Math.floor(Math.random() * 4);
       v = {
-          x: (Math.floor(Math.random() * MAP_SIZE) - PLATFORM_SIZE) * PLATFORM_SIZE,
-          y: 1,
-          z: (Math.floor(Math.random() * MAP_SIZE) - PLATFORM_SIZE) * PLATFORM_SIZE
+          x: (Math.floor(Math.random() * MAP_SIZE) - PLATFORM_SIZE) * PLATFORM_SIZE+1,
+          y: Math.floor(Math.random() * 2),
+          z: (Math.floor(Math.random() * MAP_SIZE) - PLATFORM_SIZE) * PLATFORM_SIZE+1
       };
+
+      if(wallnumber == 1){
+        createrockwall(mazeWall1,v.x,v.y,v.z);
+      }else if(wallnumber == 2){
+        createrockwall(mazeWall2,v.x,v.y,v.z);
+      }else if(wallnumber == 3){
+        createrockwall(mazeWall3,v.x,v.y,v.z);
+      }else{
+        createrockwall(mazeWall4,v.x,v.y,v.z);
+      }
+
       rock = document.createElement('a-dodecahedron');
       maze.appendChild(rock);
       rock.setAttribute('src','#stone');
-      rock.setAttribute('radius','3');
-      rock.setAttribute('position', v.x + ' ' + v.y + ' ' + v.z);
-      rock.setAttribute('static-body', '');
+      rock.setAttribute('radius','0.5');
+      rock.setAttribute('position', v.x + ' ' + 0 + ' ' + v.z);
+      rock.setAttribute('dynamic-body', '');
   }
+  // var treasureBox = document.createElement('a-box');
+  // maze.appendChild(treasureBox);
+  // treasureBox.setAttribute('check','');
+  var treasureBox1 = document.querySelector('#treasureBox1');
+  treasureBox1.setAttribute('position', ''+treasureXPos + ' ' + 2.5 + ' ' + treasureZPos + '');
 }
 
-
+function createrockwall(wall,x,y,z){
+     var wallrock = document.createElement('a-dodecahedron');
+     wall.appendChild(wallrock);
+     wallrock.setAttribute('src','#stone');
+     wallrock.setAttribute('radius','3');
+     wallrock.setAttribute('position', x + ' ' + y + ' ' + z);
+     wallrock.setAttribute('static-body', '');
+}
 
 // treasure point
 
 function addtreasure(x,z){
-
+  var showMessage = document.querySelector('#showMessage');
+  var treasureAlert = document.querySelector('#treasureAlert');
   if(x<=treasureXPos && z>=treasureZPos){
-    alert('找到絕世詩集了');
+    // console.log('絕世詩集在附近');
+    treasureCheck = true;
+    treasureAlert.setAttribute('visible',true);
+  }else{
+    treasureCheck = false;
+    treasureAlert.setAttribute('visible',false);
+    // showMessage.setAttribute('visible',false);
   }
+  
 
 }
 
@@ -72,16 +123,19 @@ function addtreasure(x,z){
 AFRAME.registerComponent('check', {
   init:function(){
     var element = this.el;
-    console.log('ok');
-    
+    var showMessage = document.querySelector('#showMessage');
+    var message = document.querySelector('#message');
     this.el.addEventListener('click',function(){
           hoverCheck = true;
-          console.log(element,'check');
+          showMessage.setAttribute('visible',true);
+          message.setAttribute('value','GET TREASURE !!!');
     });
   }
 });
-//effect
 
+
+
+//discover effect function
 AFRAME.registerComponent('effect',{
   init:function(){
     var effectBox = document.querySelector('#effect');
@@ -99,6 +153,20 @@ AFRAME.registerComponent('effect',{
       });
   }
 });
+
+//reland
+AFRAME.registerComponent('reland',{
+  init:function(){
+    this.el.addEventListener('click',function(){
+      showMessage.setAttribute('visible',true);
+      message.setAttribute('value','reload?!');
+      setTimeout(function(){
+        location.reload();
+      },1000);
+    });
+  }
+});
+
 // auto move
 AFRAME.registerComponent('automove-controls', {
   init: function () {
@@ -106,13 +174,13 @@ AFRAME.registerComponent('automove-controls', {
       this.isMoving = false;
       this.velocityDelta = new THREE.Vector3();
   },
-  tick:function(){
-    if(hoverCheck == true){
-      this.isMoving = true;
-    }else{
-      this.isMoving = false;
-    }
-  },
+  // tick:function(){
+  //   if(hoverCheck == true){
+  //     this.isMoving = true;
+  //   }else{
+  //     this.isMoving = false;
+  //   }
+  // },
   isVelocityActive: function () {
       return this.isMoving;
   },
@@ -146,7 +214,9 @@ AFRAME.registerComponent('listener', {
       currntposX = this.el.getAttribute('position').x;
       currntposY = this.el.getAttribute('position').y;
       addtreasure(currntposX,currntposZ);
-      // console.log(this.el.getAttribute('position'),'positon');
+      console.log(treasureXPos,'posx');
+      console.log(treasureZPos,'posZ');
+      //console.log(this.el.getAttribute('position'),'positon');
       
     }
   });
